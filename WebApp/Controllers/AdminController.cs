5098808542573da6905d1d59e2fb725c30b643fd
Mona Khorasani <monakhrsn@gmail.com>
2025-03-22 10:54:11 +0100

    Develop ClienForm, Add bootstrap link to layouts, develop clientList, develop edit client form, develop AddClientForm model, developing endpoints for add and edit client,Styling, develop open and close modal in javascript

--- WebApp/Controllers/AdminController.cs
+++ WebApp/Controllers/AdminController.cs
@@ -1,3 +1,4 @@
+using Business.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApp.Controllers;
@@ -6,16 +7,30 @@ namespace WebApp.Controllers;
 public class AdminController : Controller
 {
     [Route("members")]
-    // 
     public IActionResult Members()
     {
         return View();
     }
     
     [Route("clients")]
-    // 
     public IActionResult Clients()
     {
+        return View();
+    }
+    
+    [HttpPost]
+    public IActionResult AddClient(AddClientForm form)
+    {
+        if(!ModelState.IsValid)
+            return RedirectToAction("Clients");
+        return View();
+    }
+    
+    [HttpPut]
+    public IActionResult EditClient(AddClientForm form)
+    {
+        if(!ModelState.IsValid)
+            return RedirectToAction("Clients");
         return View();
     }
     
