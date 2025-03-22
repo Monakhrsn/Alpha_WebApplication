e1a3ef874cee89fb2d96f617105350fbeb051d44
Mona Khorasani <monakhrsn@gmail.com>
2025-03-22 12:46:01 +0100

    develop handle image priviewr in javascrip and fix styling

--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -1,4 +1,5 @@
 ﻿document.addEventListener("DOMContentLoaded", () => {
+    const previewSize = 150
     
     // open modal
     const modalButtons = document.querySelectorAll('[data-modal="true"]')
@@ -26,4 +27,49 @@
         })
     })
     
+    // handle image previewer
+    document.querySelectorAll('.image-previewer').forEach(previewer => {
+        const fileInput = previewer.querySelector('input[type="file"]')
+        const imagePreview = previewer.querySelector('.image-preview')
+        
+        previewer.addEventListener('click', () => fileInput.click())
+        
+        fileInput.addEventListener('change', ({ target: { files } }) => {
+          const file = files[0]
+          if (file)
+              processImage(file, imagePreview, previewer, previewSize)
+        })
+    })
 })
+
+async function loadImage(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader()
+        
+        reader.onerror = () => reject(new Error("Failed to load file."))
+        reader.onload = (e) => {
+            const img = new Image()
+            img.onerror = () => reject(new Error("Filled to load image."))
+            img.onload = () => resolve(img)
+            img.src = e.target.result
+        }
+        
+        reader.readAsDataURL(file)
+    })
+}
+
+async function processImage(file, imagePreview, previewer, previewSize = 150) {
+    try {
+        const img = await loadImage(file)
+        const canvas = document.createElement('canvas')
+        canvas.width = previewSize
+        canvas.height = previewSize
+        
+        const ctx = canvas.getContext('2d')
+        ctx.drawImage(img, 0, 0, previewSize, previewSize)
+        imagePreview.src = canvas.toDataURL('image/jpeg')
+        previewer.classList.add('selected')
+    } catch (error){
+        console.error('Failed on image processing', error)
+    }
+}
