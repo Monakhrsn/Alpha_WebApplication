33ba2a0fa9e0e0e957a95c972cce0b222cba4676
Mona Khorasani <monakhrsn@gmail.com>
2025-04-17 17:27:12 +0200

    developing dropdowns

--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -1,4 +1,35 @@
-﻿document.addEventListener("DOMContentLoaded", () => {
+﻿const dropdowns = document.querySelectorAll('[data-type="dropdown"]')
+
+document.addEventListener('click', function (event) {
+    let clickedDropdown = null
+    dropdowns.forEach(dropdown => {
+        const targetId = dropdown.getAttribute('data-target')
+        const targetElement = document.querySelector(targetId)
+        console.log(targetElement, targetId)
+
+        if (dropdown.contains(event.target)) {
+            clickedDropdown = targetElement
+            
+            document.querySelectorAll('.dropdown.dropdown-show').forEach(openDropdown => {
+                if (openDropdown !== targetElement) {
+                    openDropdown.classList.remove('dropdown-show')
+                }
+            })
+
+            targetElement.classList.toggle('dropdown-show')
+        }
+    })
+
+    if (!clickedDropdown && !event.target.closest('.dropdown')) {
+        document.querySelectorAll('.dropdown.dropdown-show').forEach(openDropdown => {
+            openDropdown.classList.remove('dropdown-show')
+        })
+    }
+})
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
     const previewSize = 150
     
     // open modal
